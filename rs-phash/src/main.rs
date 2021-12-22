8a38ec0ac270b773353a0f2c7e488ecbe46a7d22
Mickey Sola <micksola@cisco.com>
2021-12-21 19:41:23 -0500

    updating with suggestions from scott; also adding content-based decoding that ignores file extensions

--- rs-phash/src/main.rs
+++ rs-phash/src/main.rs
@@ -1,71 +1,74 @@
 extern crate image;
 
+use anyhow::{anyhow, Result};
 use image::imageops::FilterType::Lanczos3;
 use rustdct::DctPlanner;
+use std::path::{Path, PathBuf};
+use structopt::StructOpt;
 
-use std::env;
-use std::process;
-fn main() {
+#[derive(Debug, StructOpt)]
+struct Opt {
+    #[structopt(short, long)]
+    debug: bool,
+    
+    #[structopt(name = "IMG_FILE", required = true)]
+    img_path: Vec<PathBuf>,
 
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() < 2 {
-        process::exit(2);
-    }
-
-    for i in 1..args.len() {
-        let path = &args[i];
-        print_phash_from_img_path(path);
-    }
 }
 
-fn print_phash_from_img_path(path: &String) { 
-    let res = image::open(path);
-    let image1;
-    match res {
-        Ok(img) => image1 = img,
-        Err(error) => {
-            println!("error decoding image... path: {:?}, err: {:?}", path, error);
-            return ();
-        },
-    };
+pub fn main() -> Result<()> {
+    let opt = Opt::from_args();
+    let include_filename = opt.img_path.len() > 1;
+    let debug = opt.debug;
+    for path in &opt.img_path {
+        print_phash_from_img_path(path, include_filename, debug)?;
+    }
+    Ok(())
+}
 
-    // convert image to grayscale, then resize
-    let image1 = image1.grayscale();
-    let imgbuff_tmp = image::DynamicImage::resize_exact(&image1, 32, 32, Lanczos3).to_bytes();
+fn print_phash_from_img_path(path: &Path, include_filename: bool, debug: bool) -> Result<()> {
+
+    let image_gs = image::io::Reader::open(&path)
+        .map_err(|e| anyhow!("opening {:?}: {}", &path, e))?
+        .with_guessed_format()
+        .map_err(|e| anyhow!("guessing type {:?}: {}", &path, e))?
+        .decode()
+        .map_err(|e| anyhow!("decoding {:?}: {}", &path, e))?
+        // convert image to grayscale, then resize
+        .grayscale();
+
+    let imgbuff_tmp = image::DynamicImage::resize_exact(&image_gs, 32, 32, Lanczos3).to_bytes();
     let mut imgbuff: Vec<f32> = imgbuff_tmp.into_iter().map(|x| x as f32).collect();
 
-    // perform discrete cosine transform (dct2) on image 
-    let mut planner= DctPlanner::new(); 
-    let dct2 = planner.plan_dct2(1024);
-
-    dct2.process_dct2(&mut imgbuff);
-
+    // perform discrete cosine transform (dct2) on image
+    DctPlanner::new().plan_dct2(1024).process_dct2(&mut imgbuff);
 
     // construct a low frequency dct vector using the topmost 8x8 terms
     let mut dct_low_freq: Vec<f32> = Vec::new();
 
     for i in 0..8 {
         let idx = i * 32;
-        dct_low_freq.extend_from_slice(&imgbuff[idx..idx+8]);
+        dct_low_freq.extend_from_slice(&imgbuff[idx..idx + 8]);
     }
 
-    // take the low frequency averages, exlcuding the first term
-    let sum: f32= dct_low_freq.iter().sum();
+    // take the low frequency averages, excluding the first term
+    let sum: f32 = dct_low_freq.iter().sum();
     let sum = sum - dct_low_freq[0];
     let mean = sum / (dct_low_freq.len() - 1) as f32;
 
     // construct hash vector by reducing DCT values to 1 or 0 by comparing terms vs mean
-    let hashvec: Vec<u64> = dct_low_freq.into_iter()
-                                        .map(|x| 
-                                            if x > mean {1} else {0}
-                                        ).collect();
+    let hashvec: Vec<u64> = dct_low_freq
+        .into_iter()
+        .map(|x| if x > mean { 1 } else { 0 })
+        .collect();
 
     // construct hash integer from bits
-    let hash: u64 = hashvec.iter()
-                            .fold(0, |res, &bit| { 
-                                (res << 1) ^ bit} 
-                            );
-    
-    println!("{:#x}", hash);    
+    let hash: u64 = hashvec.iter().fold(0, |res, &bit| (res << 1) ^ bit);
+
+    if include_filename && debug {
+        println!("{:?}: {:#x}", &path, hash);
+    } else {
+        println!("{:#x}", hash);
+    }
+    Ok(())
 }
